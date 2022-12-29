3cc6c95e4fc2a455eea632838d9130e59435cb8f
maopineda <ingmauriciopineda@gmail.com>
2022-12-29 15:07:31 -0500

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Books';
+  book: string = '';
+  genre: string = '';
+  genres = [
+    {id: 1, name: 'Poesía'},
+    {id: 2, name: 'Novela'},
+    {id: 3, name: 'Ciencia-Ficción'}
+  ];
+  books: any = [];
+
+  findGnreById(id: number) {
+    let foundGenre = this.genres.find(g => g.id == id)
+    console.log(foundGenre);
+    return foundGenre;
+  }
+
+  onSubmit(e: Event) {
+    e.preventDefault();
+
+    //let genre = this.findGnreById(this.selectedGenre);
+    let formData = {
+      name: this.book,
+      genre: this.findGnreById(+this.genre)?.name
+    }
+    console.log(formData);
+    this.books.push(formData);
+    console.log(this.books);
+  }
 }
